891f451a6edc7a49efb377d107ea07e02f5c990c
RicharDev01 <ricardopineda587@gmail.com>
2022-10-23 22:51:50 -0600

    Se realiza diseño preliminar de Form Login

--- Vistas/FLogin.Designer.cs
+++ Vistas/FLogin.Designer.cs
@@ -29,22 +29,122 @@ namespace Vistas
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FLogin));
+      this.panelTitleBar = new System.Windows.Forms.Panel();
+      this.btnCerrar = new FontAwesome.Sharp.IconButton();
+      this.btnMinimizar = new FontAwesome.Sharp.IconButton();
+      this.panelBgInicio = new System.Windows.Forms.Panel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.panelTitleBar.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       this.SuspendLayout();
       // 
-      // Form1
+      // panelTitleBar
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(59)))), ((int)(((byte)(79)))));
+      this.panelTitleBar.Controls.Add(this.pictureBox1);
+      this.panelTitleBar.Controls.Add(this.btnCerrar);
+      this.panelTitleBar.Controls.Add(this.btnMinimizar);
+      this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+      this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+      this.panelTitleBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.panelTitleBar.Name = "panelTitleBar";
+      this.panelTitleBar.Size = new System.Drawing.Size(1280, 34);
+      this.panelTitleBar.TabIndex = 0;
+      this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
+      // 
+      // btnCerrar
+      // 
+      this.btnCerrar.BackColor = System.Drawing.Color.Transparent;
+      this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btnCerrar.Dock = System.Windows.Forms.DockStyle.Right;
+      this.btnCerrar.FlatAppearance.BorderSize = 0;
+      this.btnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+      this.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+      this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.btnCerrar.IconChar = FontAwesome.Sharp.IconChar.XmarkSquare;
+      this.btnCerrar.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(102)))), ((int)(((byte)(64)))));
+      this.btnCerrar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+      this.btnCerrar.IconSize = 38;
+      this.btnCerrar.Location = new System.Drawing.Point(1244, 0);
+      this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.btnCerrar.Name = "btnCerrar";
+      this.btnCerrar.Size = new System.Drawing.Size(36, 34);
+      this.btnCerrar.TabIndex = 2;
+      this.btnCerrar.UseVisualStyleBackColor = false;
+      this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+      // 
+      // btnMinimizar
+      // 
+      this.btnMinimizar.BackColor = System.Drawing.Color.Transparent;
+      this.btnMinimizar.FlatAppearance.BorderSize = 0;
+      this.btnMinimizar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+      this.btnMinimizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+      this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.btnMinimizar.IconChar = FontAwesome.Sharp.IconChar.SquareMinus;
+      this.btnMinimizar.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(133)))), ((int)(((byte)(225)))));
+      this.btnMinimizar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+      this.btnMinimizar.IconSize = 38;
+      this.btnMinimizar.Location = new System.Drawing.Point(1200, 0);
+      this.btnMinimizar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.btnMinimizar.Name = "btnMinimizar";
+      this.btnMinimizar.Size = new System.Drawing.Size(36, 34);
+      this.btnMinimizar.TabIndex = 1;
+      this.btnMinimizar.UseVisualStyleBackColor = false;
+      this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+      // 
+      // panelBgInicio
+      // 
+      this.panelBgInicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(51)))), ((int)(((byte)(48)))), ((int)(((byte)(60)))));
+      this.panelBgInicio.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panelBgInicio.Location = new System.Drawing.Point(0, 34);
+      this.panelBgInicio.Name = "panelBgInicio";
+      this.panelBgInicio.Size = new System.Drawing.Size(1280, 686);
+      this.panelBgInicio.TabIndex = 1;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(42, 34);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBox1.TabIndex = 3;
+      this.pictureBox1.TabStop = false;
+      // 
+      // FLogin
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-      this.Name = "Form1";
-      this.Text = "Form1";
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(48)))), ((int)(((byte)(60)))));
+      this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+      this.ClientSize = new System.Drawing.Size(1280, 720);
+      this.Controls.Add(this.panelBgInicio);
+      this.Controls.Add(this.panelTitleBar);
+      this.Font = new System.Drawing.Font("Poppins", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+      this.ForeColor = System.Drawing.Color.White;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.Name = "FLogin";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+      this.Text = "Inicio de Sesión";
+      this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(178)))), ((int)(((byte)(148)))));
+      this.panelTitleBar.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
       this.ResumeLayout(false);
 
     }
 
     #endregion
 
+    private System.Windows.Forms.Panel panelTitleBar;
+    private FontAwesome.Sharp.IconButton btnCerrar;
+    private FontAwesome.Sharp.IconButton btnMinimizar;
+    private System.Windows.Forms.Panel panelBgInicio;
+    private System.Windows.Forms.PictureBox pictureBox1;
   }
 }
 
--- Vistas/FLogin.cs
+++ Vistas/FLogin.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +17,54 @@ namespace Vistas
     {
       InitializeComponent();
     }
+
+    // creo un struct para guardar la paleta de colores
+    struct PaletaDeColores
+    {
+      public static Color bgColor = Color.FromArgb(30,30,30); 
+      public static Color bgColor2 = Color.FromArgb(61, 59, 79);   
+      public static Color bgColorAccent = Color.FromArgb(82, 81, 96);   
+      public static Color mainColor = Color.FromArgb(87,86,204);  
+      public static Color secundaryColor = Color.FromArgb(129, 133, 225);   
+      public static Color white = Color.FromArgb(255,255,255);
+      public static Color seccesssColor = Color.FromArgb(0, 178, 148);
+      public static Color alertColor = Color.FromArgb(209, 102, 64);
+      public static Color activeColor = Color.FromArgb(223, 126, 231);
+      public static Color textColor = Color.FromArgb(170, 168, 195);
+      public static Color infoColor = Color.FromArgb(103, 72, 213); 
+    }
+
+    //Esto me ayuda a dar movimiento de arrastre al form
+    // sin la barra de widows que viene por defecto
+    // de esta manera puedo personalizar mi interfaz
+    [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+    private extern static void ReleaseCapture();
+    [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+    private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+    private void btnCerrar_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+
+    private void btnMinimizar_Click(object sender, EventArgs e)
+    {
+      WindowState = FormWindowState.Minimized;
+    }
+
+    private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
+    {
+      ReleaseCapture();
+      SendMessage(this.Handle, 0x112, 0xf012, 0);
+    }
+
+
+
+    //private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
+    //{
+    //  ReleaseCapture();
+    //  SendMessage(this.Handle, 0x112, 0xf012, 0);
+    //}
 
   }
 }
